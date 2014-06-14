25e5025f496b6a2f146ef48b94d55db0213fbbec
Seth Ammons <seth.ammons@sendgrid.com>
2014-06-14 16:01:14 -0700

    _meta endpoints functional

--- main.go
+++ main.go
@@ -182,23 +182,96 @@ func (a *Apid) DeleteTable(w http.ResponseWriter, r *http.Request, t httprouter.
 	}
 }
 
-func (a *Apid) TableMetaHandler(w http.ResponseWriter, r *http.Request, t httprouter.Params) {
-	log.Print("TableMetaHandler")
+type Meta struct {
+	Description string              `json:"description"`
+	SchemaType  string              `json:"type"`
+	Properties  map[string]Property `json:"properties"`
+	Required    []string            `json:"required"`
+	Primary     string              `json:"primary"`
+	Location    string              `json:"location"`
+	Methods     []string            `json:"methods"`
+	Title       string              `json:"title"`
+}
 
-	table := t.ByName("table")
-	w.Write([]byte(fmt.Sprintf("request for table %s meta data ", table)))
-	if _, ok := a.Tables[table]; ok {
-		log.Println("Table found! ")
-	} else {
-		log.Println("No table found ")
-		NotFoundWithParams(w, r, "")
+type Property struct {
+	Description string `json:"description"`
+	DataType    string `json:"type"`
+}
+
+func (a *Apid) TableMetaHandler(w http.ResponseWriter, r *http.Request, t httprouter.Params) {
+	tableName := t.ByName("table")
+	if _, ok := a.Tables[tableName]; !ok {
+		NotFoundWithParams(w, r, fmt.Sprintf("No table (%s) found for _meta", tableName))
 		return
 	}
+
+	log.Printf("200 - %s %s", r.Method, r.RequestURI)
+	w.Header().Set("Content-Type", "application/json")
+	location := r.RequestURI[:len(r.RequestURI)-len("_meta")]
+	j, err := json.Marshal(GenMeta(a.Tables[tableName], location))
+	if err != nil {
+		log.Printf("error making json schema ", err)
+	}
+	w.Write(j)
 }
 
 func (a *Apid) MetaHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	log.Print("metaHandler")
-	w.Write([]byte("meta for whole schema"))
+	wholeSchema := make([]Meta, 0)
+	for _, t := range a.Tables {
+		location := r.RequestURI[:len(r.RequestURI)-len("_meta")] + t.Name
+		wholeSchema = append(wholeSchema, GenMeta(t, location))
+	}
+	log.Printf("200 - %s %s", r.Method, r.RequestURI)
+	w.Header().Set("Content-Type", "application/json")
+	j, err := json.Marshal(wholeSchema)
+	if err != nil {
+		log.Printf("error making json whole schema ", err)
+	}
+	w.Write(j)
+}
+
+func GenMeta(table *Table, location string) Meta {
+	// this could all be initialized at startup, as oppposed to each call
+	properties := make(map[string]Property)
+	required := make([]string, 0)
+	primary := ""
+	schemaType := "object"
+	// assume all methods for now. latter, we can have a table restrictions
+	methods := []string{"GET", "POST", "PUT", "DELETE"}
+
+	// get column info for properties
+	for _, c := range table.Cols {
+		p := Property{}
+		p.DataType = c.DATA_TYPE.String
+		p.Description = c.COLUMN_COMMENT.String
+
+		if c.COLUMN_KEY.String == "PRI" {
+			log.Print("setting primary key")
+			primary = c.COLUMN_NAME.String
+		}
+
+		if c.IS_NULLABLE.String == "NO" {
+			required = append(required, c.COLUMN_NAME.String)
+		}
+
+		properties[c.COLUMN_NAME.String] = p
+	}
+	properties["limit"] = Property{DataType: "int", Description: "Used to limit the number of results returned"}
+	properties["offset"] = Property{DataType: "int", Description: "Used to offset results returned"}
+
+	// init schema struct
+	schema := Meta{}
+	schema.Title = table.Name
+	schema.Properties = properties
+	schema.Description = "MySQL Table " + table.Name
+	schema.Location = location
+	schema.Primary = primary
+	schema.Required = required
+	schema.SchemaType = schemaType
+	schema.Methods = methods
+
+	return schema
 }
 
 // refactor to take interface with methods *.URL.RawQuery
